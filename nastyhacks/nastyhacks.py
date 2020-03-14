f1020eaf2b99f5d763e74bda0132c2cc698844d3
ubermenzchen <matheus_garcias@hotmail.com>
2020-03-14 12:01:30 -0300

    Colocando informações

--- nastyhacks/nastyhacks.py
+++ nastyhacks/nastyhacks.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python3
+"""
+Autor: Matheus da Silva Garcias
+Matrícula: 20171BSI0456
+Problema: https://open.kattis.com/problems/nastyhacks
+Resultado: https://open.kattis.com/submissions/5463684
+"""
 
 from sys import stdin, stdout
 
